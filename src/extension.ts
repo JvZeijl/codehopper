56ded365f4269cfde68dafc5480b1032f8c78168
JvZeijl <joostvzeijl4@gmail.com>
2024-08-18 08:41:54 +0200

    update codehopper instance on settings change

--- src/extension.ts
+++ src/extension.ts
@@ -2,12 +2,17 @@ import * as vscode from 'vscode';
 import { Codehopper } from './codehopper';
 
 export function activate(context: vscode.ExtensionContext) {
-	const codehopper = new Codehopper(context);
+	let codehopper = new Codehopper(context);
+	
+	const settingsListener = vscode.workspace.onDidChangeConfiguration(changeEvent => {
+		if (changeEvent.affectsConfiguration('codehopper')) codehopper = new Codehopper(context);
+	});
+
 	const openHopperCommand = vscode.commands.registerTextEditorCommand('codehopper.openHopper', textEditor => {
 		codehopper.open(textEditor);
 	});
 
-	context.subscriptions.push(openHopperCommand);
+	context.subscriptions.push(openHopperCommand, settingsListener);
 }
 
 export function deactivate() {
